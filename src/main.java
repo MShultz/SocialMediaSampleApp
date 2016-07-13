c21c7ba0aa431196197fd9139f5fb8a40ce59526
MShultz <mshultz@student.neumont.edu>
2016-07-13 10:30:23 -0600

    Initial Commit of Social Media Application

---
+++ src/main.java
@@ -0,0 +1,18 @@
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+
+public class main {
+
+	public static void main(String[] args) {
+		Cluster cluster;
+		Session session;
+		
+		
+
+	}
+	public void openSession(){
+		
+	}
+
+}
