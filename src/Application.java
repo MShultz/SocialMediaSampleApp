4292cb7aeb3ba3201aabbec14d4681d389dba32b
MShultz <mshultz@student.neumont.edu>
2016-07-13 11:46:39 -0600

    Connection to node achieved.

---
+++ src/Application.java
@@ -0,0 +1,51 @@
+import com.datastax.driver.core.Cluster;
+import com.datastax.driver.core.Session;
+
+public class Application {
+	Cluster.Builder cluster;
+	Session session;
+	
+	
+	public Application(){
+		cluster = Cluster.builder().addContactPoint("13.91.46.223").withPort(9042).withCredentials("cassandra", "bitnami");
+		session = cluster.build().connect();
+		createApplication("SM");
+		shutdownConnection();
+		
+	}
+	public void createApplication(String keySpace){
+		createKeySpace(keySpace);
+		createSchema();
+	}
+	private void createKeySpace(String keySpace){
+		String cqlStatement = "CREATE KEYSPACE " + keySpace + " WITH replication = "
+				+ " {'class':'SimpleStrategy','replication_factor':1}";        
+		session.execute(cqlStatement);
+	}
+	private void createSchema(){
+		String use = "Use SM";
+		session.execute(use);
+		
+		String createUsers = "CREATE TABLE SM.users (" + 
+                " user_name varchar PRIMARY KEY," + 
+                " password varchar " + 
+                ")";
+		String createFollowing = "CREATE TABLE SM.following (" + 
+                " user_name varchar PRIMARY KEY," + 
+                " followed_user_Names list<text> " + 
+                ")";
+		String createPostWall = "CREATE TABLE SM.posts (" + 
+                " post_ID timestamp," + 
+                " user_Name varchar, " + 
+                " content text,"+
+                "PRIMARY KEY (post_ID, user_Name))";
+
+		session.execute(createUsers);
+		session.execute(createFollowing);
+		session.execute(createPostWall);
+		
+	}
+	private void shutdownConnection(){
+		session.close();
+	}
+}
--- src/main.java
+++ src/main.java
@@ -1,17 +1,7 @@
-import com.datastax.driver.core.Cluster;
-import com.datastax.driver.core.Session;
 
-
-public class main {
-
+public class main {	
 	public static void main(String[] args) {
-		Cluster cluster;
-		Session session;
-		
-		
-
-	}
-	public void openSession(){
+		Application app = new Application();
 		
 	}
 
